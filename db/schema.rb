f10a795181028e593a81eb17ebbef03439c0661e
jean-rodriguez kinouani <kinouani@gmail.com>
2022-10-31 20:14:12 +0100

    add model city

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_154504) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_190924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "region"
+    t.string "state"
+    t.integer "safety"
+    t.integer "cost"
+    t.integer "sightseeing"
+    t.integer "vibe"
+    t.integer "culture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
