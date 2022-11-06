aa0a0c97056ce4b09c6b9ecdea6f12c65e629d8c
jean-rodriguez kinouani <kinouani@gmail.com>
2022-11-06 20:56:50 +0100

    add design page show

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_161319) do
     t.integer "culture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover"
+    t.integer "food"
   end
 
   create_table "users", force: :cascade do |t|
